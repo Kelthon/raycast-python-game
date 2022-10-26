75ecce9cfa17a23bd6426272cfb97aa08b60d3d3
Kelthon <kelthonbalbino@gmail.com>
2022-10-26 11:47:46 -0300

    refatored raycast

--- game/src/raycast.py
+++ game/src/raycast.py
@@ -8,34 +8,36 @@ class Raycast():
         self.color = color
         self.shape = draw.aaline(global_window.surface, self.color, self.origin, self.raycast())
 
-    def raycast(self):
+    def line_slope(self) -> float:
+        slope = 0
+
+        if self.direction.x != self.origin.x and self.direction.y != self.origin.y:
+            slope_direction = self.origin.y - self.direction.y if self.direction.y < self.origin.y else self.direction.y - self.origin.y
+            slope = slope_direction / (self.direction.x - self.origin.x)
+
+        return slope
+
+    def raycast(self, max_length: Vector2 = Vector2(global_window.width, global_window.height)):
         # Calc raycast line
         ray = Vector2(0, 0)
-        line_slope = 0
+        line_slope = self.line_slope()
 
-        # Case line slope is zero
+        # Case line slope is zero and raycast is a horizontal line
         if self.direction.x == self.origin.x:
             ray.x = self.direction.x
-            if self.direction.y > self.origin.y:
-                ray.y = global_window.height
-            else:
-                ray.y = 0
+            ray.y = max_length.y if self.direction.y > self.origin.y else 0
+        
+        # Case line slope is zero and raycast is a vertical line
         elif self.direction.y == self.origin.y:
             ray.y = self.direction.y
-            if self.direction.x > self.origin.x:
-                ray.x = global_window.width
-            else:
-                ray.x = 0
-        # Case line slope is no-zero
+            ray.x = max_length.x if self.direction.x > self.origin.x else 0
+        
+        # Case line slope is no-zero and raycast is a sloped line
         else:
-            if self.direction.y < self.origin.y:
+            ray.y = 0 if self.direction.y < self.origin.y else max_length.y
+            slope_direction = (self.direction.y - ray.y) if self.direction.y < self.origin.y else (ray.y - self.direction.y)
+            ray.x = self.direction.x + (slope_direction / line_slope)
 
-                ray.y = 0
-                line_slope = (self.origin.y - self.direction.y) / (self.direction.x - self.origin.x)
-                ray.x = self.direction.x + ((self.direction.y - ray.y) / line_slope)
-
-            else:
-                ray.y = global_window.height
-                line_slope = (self.direction.y - self.origin.y) / (self.direction.x - self.origin.x)
-                ray.x = self.direction.x + ((ray.y - self.direction.y) / line_slope)
         return ray
+
+    
