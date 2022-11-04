6093c6415e774aa313911924d24b1fbc47e9400a
Kelthon <kelthonbalbino@gmail.com>
2022-11-04 17:16:11 -0300

    added raycast with collision

--- game/src/scene.py
+++ game/src/scene.py
@@ -8,11 +8,12 @@ class Scene():
     def __init__(self) -> None:
         self.enemies: List[Entity] = []
         self.map: List[Rect] = []
-        self.player = player 
+        self.player = player
         self.skybox = None
         self.items = []
 
     def load_map(self):
+        # refact this
         self.map = [
             draw.rect(global_window.surface, Color(255, 255, 255), (50, 0, 50, 100)),
             draw.rect(global_window.surface, Color(255, 255, 255), (600, 50, 50, 300)),
@@ -26,7 +27,7 @@ class Scene():
             draw.rect(global_window.surface, Color(255, 255, 255), (100, 350, 25, 25)),
             draw.rect(global_window.surface, Color(255, 255, 255), (50, 400, 25, 25)),
             draw.rect(global_window.surface, Color(255, 255, 255), (100, 400, 25, 25)),
-            draw.circle(global_window.surface, Color(255, 255, 255), Vector2(400, 80), 50),
+            draw.rect(global_window.surface, Color(255, 255, 255), (400, 30, 50, 100)),
             draw.rect(global_window.surface, Color(255, 255, 255), (245, 180, 100, 40)),
             draw.rect(global_window.surface, Color(255, 255, 255), (375, 250, 40, 40)),
             draw.rect(global_window.surface, Color(255, 255, 255), (305, 250, 40, 40))
@@ -47,10 +48,19 @@ class Scene():
 
     def draw_scene(self):
         self.load_map()
-        
-        raycast = Raycast(Vector2(self.player.transform.position.x + self.player.transform.scale.x / 2, self.player.transform.position.y + self.player.transform.scale.y / 2), Vector2(Mouse.get_pos()), Color(0, 255, 0))
-        raycast.shape
 
+        ray_origin = Vector2(
+            self.player.transform.position.x + self.player.transform.scale.x / 2,
+            self.player.transform.position.y + self.player.transform.scale.y / 2
+        )
+
+        raycast = Raycast(
+            origin=ray_origin, 
+            direction=Vector2(Mouse.get_pos()), 
+            color=Color(0, 255, 0), 
+            collision_list=self.map
+        )
+        
         self.player.update(self.map)
         global_window.surface.blit(self.player.shape, self.player.transform.position)
 
