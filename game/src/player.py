4c6982dd2a223cfeb00b6cd0a0adeff0192cd149
Kelthon <kelthonbalbino@gmail.com>
2022-12-09 13:58:35 -0300

    modularized player functions, and collision fuctions

---
+++ game/src/player.py
@@ -0,0 +1,155 @@
+# from game.src.facade import *
+from facade import *
+from typing import List, overload
+
+class Walker(object):
+    def __init__(self, position, speed, size) -> None:
+        self.position: Vector2 = position
+        self.speed: float = speed
+        self.size: Vector2 = size
+
+    def move(self, direction: Vector2):
+        self.position.x += self.speed * direction.x 
+        self.position.y += self.speed * direction.y
+    
+
+class Collider(object):
+    top = 1
+    down = 2
+    left = 3
+    right = 4
+
+    def __init__(self) -> None:
+        self.colliders = []
+
+    def get_colliders(self):
+        return self.colliders
+
+    @overload
+    def add(self, rect: Rect):
+        ...
+
+    @overload
+    def add(self, rectlist: List[Rect]):
+        ...
+
+    def add(self, obj: Rect | List[Rect]):
+        if isinstance(obj, Rect):
+            self.colliders.append(obj)
+        elif isinstance(obj, list):
+            for rect in obj:
+                if isinstance(rect, Rect):
+                    self.colliders.append(rect)
+                else:
+                    raise TypeError("Insertion Failure: List with disallowed types in " + rect + " of type " + type(rect))
+        else:
+            raise TypeError("Invalid types: rectlist argument must be of type Rect or List[Rect]")
+
+    @overload
+    def remove(self, rect: Rect):
+        ...
+
+    @overload
+    def remove(self, index: int):
+        ...
+
+    @overload
+    def remove(self, rectlist: List[Rect]):
+        ...
+
+    def remove(self, obj: Rect | int | List[Rect]) -> None:
+        if isinstance(obj, Rect):
+            self.colliders.remove(obj)
+        elif isinstance(obj, int):
+            del self.colliders[obj]
+        elif  isinstance(obj, list):
+            for rect in obj:
+                if isinstance(rect, Rect):
+                    self.colliders.remove(rect)
+                else:
+                    raise TypeError("Remotion Failure: List with disallowed types in " + rect + " of type " + type(rect))
+        else:
+            raise TypeError("arg rectlist should be a int, Rect or List[Rect]")
+
+    def get_collision_direction(self, rect: Rect):
+        """Returns a string where has the collision with something
+
+        :params rect: represents something
+        :type rect: Rect
+
+        :returns: a int list with all collisions
+        :rtype: List[int]
+        """
+
+        collide_list = []
+
+        for collider in self.colliders:
+            if rect.colliderect(collider):
+                if rect.top <= collider.bottom and rect.bottom > collider.bottom:
+                    collide_list.append(Collider.top)
+
+                if rect.bottom >= collider.top and rect.top < collider.top:
+                    collide_list.append(Collider.down)
+                
+                if rect.right >= collider.left and rect.left < collider.left:
+                    collide_list.append(Collider.right)
+                
+                if rect.left <= collider.right and rect.right > collider.right:
+                    collide_list.append(Collider.left)
+
+        return collide_list
+
+
+class Player(Walker):
+    def __init__(self, position, speed, size, collider, binds) -> None:
+        super().__init__(position, speed, size)
+        self.collider = collider
+        self.set_binds(binds)
+        self.update()
+
+    def set_binds(self, binds):
+        self.binds = binds
+
+    def update(self, surface=display.get_surface()) -> None:
+        self.shape = draw.rect(surface, [255]*3, (self.position, self.size))
+
+    def move(self) -> None:
+        pressed_keys = key.get_pressed()
+        collisions = self.collider.get_collision_direction(self.shape)
+
+        for bind in self.binds:
+
+            pkey = bind["key"]
+            pcol = bind["col"]
+            pdir = bind["dir"]
+
+            if pressed_keys[pkey] and not pcol in collisions:
+                super().move(pdir)
+
+    def move_and_update(self) -> None:
+        self.move()
+        self.update()
+
+
+binds = [
+    {
+        "dir": Vector2(0, -1),
+        "key": local.K_w,
+        "col": Collider.top
+    },
+    {
+        "dir": Vector2(0, 1),
+        "key": local.K_s,
+        "col": Collider.down
+    },
+    {
+        "dir": Vector2(-1, 0),
+        "key": local.K_a,
+        "col": Collider.left
+    },
+    {
+        "dir": Vector2(1, 0),
+        "key": local.K_d,
+        "col": Collider.right
+    }
+]
